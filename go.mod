05e1a6e3b583c8e8693efc791a98d74e43e42662
tommy.c <tommy50101@gmail.com>
2023-04-22 23:59:50 +0800

    Able to insert block

--- go.mod
+++ go.mod
@@ -10,9 +10,12 @@ require (
 	github.com/deckarep/golang-set/v2 v2.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/holiman/uint256 v1.2.2-0.20230321075855-87b91420868c // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
@@ -20,4 +23,6 @@ require (
 	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gorm.io/driver/mysql v1.5.0 // indirect
+	gorm.io/gorm v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,88 @@
 package main
 
 import (
+	"strconv"
+	// "github.com/ethereum/go-ethereum/core/types"
+	// "math/big"
+	"log"
 	_ "github.com/joho/godotenv/autoload"
 	"os"
     "fmt"
     "github.com/ethereum/go-ethereum/ethclient"
+	"context"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"math/big"
 )
 
+type Block struct {
+	// gorm.Model
+	ID           uint
+	BlockNum     uint64
+	BlockHash    string
+	BlockTime    uint64
+	ParentHash   string
+}
+
+var(
+	db *gorm.DB
+)
+
+func (block Block) TableName() string {
+	// 绑定MYSQL表名為block
+	return "block"
+}
+
 func main() {
-	key := os.Getenv("INFURA_ETH_MAIN_KEY")
-    client, err := ethclient.Dial("https://mainnet.infura.io/v3/"+key )
+	initDb()
+	client, err := ethclient.Dial("https://mainnet.infura.io/v3/" + os.Getenv("INFURA_ETH_MAIN_KEY") )
     if err != nil {
-        fmt.Println("connection failed")
+        fmt.Println("json-rpc server connection failed")
 		return
     }
 
-	fmt.Println(*client, err);
+	// Get latest block header
+	latestHeader, err := client.HeaderByNumber(context.Background(), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // fmt.Println("Latest block header:" + latestHeader.Number.String())
+
+	// Get block by header number
+    // blockNumber := big.NewInt(17102552)
+    block, err := client.BlockByNumber(context.Background(), latestHeader.Number)
+    if err != nil {
+        log.Fatal(err)
+    }
+	fmt.Println("block_num:", block.Number().Uint64())     // 17102552
+	fmt.Println("block_hash:", block.Hash().Hex())          // 0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9
+    fmt.Println("block_time:", block.Time())       // 1527211625
+	fmt.Println("parent_num:", block.Number().Uint64() - 1 )
+
+	parentNumber := big.NewInt((block.Number().Int64() - 1))
+
+	parentBlock, err := client.BlockByNumber(context.Background(), parentNumber)
+    if err != nil {
+        log.Fatal(err)
+    }
+	parentHash := parentBlock.Hash().Hex()
+
+	blockModel := Block{
+		BlockNum: block.Number().Uint64(),
+		BlockHash: block.Hash().Hex(),
+		BlockTime: block.Time(),
+		ParentHash: parentHash,
+	}
+	db.Create(&blockModel)
+}
+
+func initDb() {
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PWD")
+	host := os.Getenv("DB_HOST")
+	port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
+	Dbname := os.Getenv("DB_NAME")
+	// 連線Db
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, Dbname)
+	db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
