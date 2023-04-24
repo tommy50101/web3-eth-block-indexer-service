4d0ebf96a6159e769028e5b47c9d8a0e18cd9a9a
tommy.c <tommy50101@gmail.com>
2023-04-24 21:46:23 +0800

    Able to batch insert logs

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var (
 	err              error
 	db               *gorm.DB
 	lastestDbBlockId uint64
-	blockId          uint64
+	lastestDbTxId    uint64
 )
 
 func main() {
@@ -57,12 +57,19 @@ func main() {
 	}
 
 	lastDbBlock := Block{}
-	db.Order("id DESC").First(&lastDbBlock)
+	db.Last(&lastDbBlock)
 	lastestDbBlockId = lastDbBlock.ID
-	blockId = lastestDbBlockId + 1
+	blockId := lastestDbBlockId + 1
+
+	lastDbTx := Transaction{}
+	db.Last(&lastDbTx)
+	lastestDbTxId = lastDbTx.ID
+	txId := lastestDbTxId + 1
 
 	blockModels := []Block{}
 	transactionModels := []Transaction{}
+	logModels := []Log{}
+
 	for {
 		// Get parent_hash by parent_num
 		parentNumber := big.NewInt((block.Number().Int64() - 1))
@@ -72,12 +79,7 @@ func main() {
 		}
 		parentHash := parentBlock.Hash().Hex()
 
-		fmt.Println("block_num:", block.Number().Uint64()) // 17102552
-		fmt.Println("block_hash:", block.Hash().Hex())     // 0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9
-		fmt.Println("block_time:", block.Time())           // 1527211625
-		fmt.Println("parent_hash:", parentHash)            // 17102551
-
-		// Insert block
+		// Arrange blocks data for insertion
 		blockModel := Block{
 			ID:         blockId,
 			BlockNum:   block.Number().Uint64(),
@@ -87,38 +89,76 @@ func main() {
 		}
 		blockModels = append(blockModels, blockModel)
 
-		// Arrange txs data for insertion
-		for _, tx := range block.Transactions() {
-			fmt.Println("block_num:", block.Number().Uint64()) // 17102552
-			fmt.Println("block_hash:", block.Hash().Hex())     // 0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9
-			fmt.Println("block_time:", block.Time())           // 1527211625
-			fmt.Println("parent_hash:", parentHash)            // 17102551
+		fmt.Println("block_num:", block.Number().Uint64()) // 17102552
+		fmt.Println("block_hash:", block.Hash().Hex())     // 0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9
+		fmt.Println("block_time:", block.Time())           // 1527211625
+		fmt.Println("parent_hash:", parentHash)            // 17102551
 
+		for _, tx := range block.Transactions() {
+			var to string
 			if tx.To() == nil {
-				continue
+				to = ""
+			} else {
+				to = tx.To().Hex()
 			}
+
+			// Arrange txs data for insertion
 			from, _ := types.Sender(types.LatestSignerForChainID(tx.ChainId()), tx)
 			transactionModel := Transaction{
+				ID:      txId,
 				TxHash:  tx.Hash().Hex(),
 				From:    from.Hex(),
-				To:      tx.To().Hex(),
+				To:      to,
 				Nonce:   tx.Nonce(),
 				Data:    tx.Data(),
 				Value:   tx.Value().String(),
 				BlockID: blockId,
 			}
 			transactionModels = append(transactionModels, transactionModel)
-		}
 
-		// Batch insert blocks, txs then exit when reached latest block
+			fmt.Println("tx_hash:", tx.Hash().Hex())   // 0x9c775841600aca0e9b4d8f1c87e3ae4fd52618d53bcc734558544c1165a6b416
+			fmt.Println("from:", from.Hex())           // 0xae2Fc483527B8EF99EB5D9B44875F005ba1FaE13
+			fmt.Println("to:", tx.To().Hex())          // 0x6b75d8AF000000e20B7a7DDf000Ba900b4009A80
+			fmt.Println("nonce:", tx.Nonce())          // 248674
+			fmt.Println("data:", tx.Data())            //
+			fmt.Println("value:", tx.Value().String()) // [169 5 156 187 0 0 0 0 0 0 0 0 0 0 0 0]
+
+			// Arrange logs data for insertion
+			receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if len(receipt.Logs) == 0 {
+				continue
+			}
+
+			// Logs
+			for _, log := range receipt.Logs {
+				fmt.Println("log_index:", log.Index) // 1
+				fmt.Println("log_data:", log.Data)   // [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 232 147 174 5 126 221 103 122 0]
+				// Insert log
+				logModel := Log{
+					Index:         log.Index,
+					Data:          log.Data,
+					TransactionID: transactionModel.ID,
+				}
+				logModels = append(logModels, logModel)
+			}
+			txId = txId + 1
+		}
+		blockId = blockId + 1
+
+		// Batch insert blocks, txs and logs, then exit when reached latest block
 		nextIndex := startIndex.Add(startIndex, big.NewInt(1))
 		nextBlock, _ := client.BlockByNumber(context.Background(), nextIndex)
 		if nextBlock != nil {
-			blockId = blockId + 1
 			block = nextBlock
 		} else {
+			// Start to batch insert all data
 			db.Create(&blockModels)
-			insertTxs(transactionModels)
+			db.Create(&transactionModels)
+			db.Create(&logModels)
 
 			log.Fatal("Has reached the latest block")
 		}
@@ -129,33 +169,4 @@ func main() {
 func initDb() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", DB_USERNAME, DB_PWD, DB_HOST, DB_PORT, DB_NAME)
 	db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-}
-
-// Insert txs
-func insertTxs(transactionModels []Transaction) {
-	db.Create(&transactionModels)
-
-	// Get logs from tx receipt
-	// receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// if len(receipt.Logs) == 0 {
-	// 	continue
-	// }
-
-	// // Logs
-	// for _, log := range receipt.Logs {
-	// 	fmt.Println("log_index:", log.Index) // 1
-	// 	fmt.Println("log_data:", log.Data)   // [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 232 147 174 5 126 221 103 122 0]
-	// 	// Insert log
-	// 	logModel := Log{
-	// 		Index:         log.Index,
-	// 		Data:          log.Data,
-	// 		TransactionID: transactionId,
-	// 	}
-	// 	db.Where(Log{Index: log.Index, Data: log.Data}).FirstOrCreate(&logModel)
-	// }
-
 }
